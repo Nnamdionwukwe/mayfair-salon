ae20234691c43e1dc454ac717ed4287686ee9d69
nnamdi onwukwe <nnamdionwukwe@gmail.com>
2025-09-18 02:07:39 +0100

    fr

--- src/components/HomeLogo.jsx
+++ src/components/HomeLogo.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 import styles from "./SalonLogo.module.css";
 
 export default function HomeLogo() {
-  const image = "salon-logo.jpg";
+  const image = "mayfair.PNG";
 
   return (
     <Link to="contacts" className={styles.homeLogo}>
       <img className={styles.logo2} src={image} />
-      <p>mayfair</p>
+      <p></p>
     </Link>
   );
 }
